c8a025bdea81fe166578db3554f9e565a4f5ae59
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2020-09-15 12:55:22 -0500

    Capturamos la edad del usuario

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 
 int main(){
     std::string nombre, apellido;
+    unsigned int edad;
     std::cout << "Hola mundo" << std::endl;
     std::cout << "Nombre: ";
     getline(std::cin, nombre);
     std::cout << "Apellido: ";
     getline(std::cin, apellido);
+    std::cout << "Edad:";
+    std::cin >> edad;
     std::cout << "Hola: " << nombre << " " << apellido << std::endl;
+    std:: cout << "Vas a cumplir: " << edad + 1 << " anios" << std::endl;
     return 0;
 }
