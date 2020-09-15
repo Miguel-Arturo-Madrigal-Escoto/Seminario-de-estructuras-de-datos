efd524c209b2d4f9a8f8ef08b59ff67d3b5df8b2
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2020-09-15 12:29:31 -0500

    Capturamos el nombre del usuario completo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main(){
+    std::string nombre, apellido;
     std::cout << "Hola mundo" << std::endl;
+    std::cout << "Nombre: ";
+    getline(std::cin, nombre);
+    std::cout << "Apellido: ";
+    getline(std::cin, apellido);
+    std::cout << "Hola: " << nombre << " " << apellido << std::endl;
     return 0;
 }
